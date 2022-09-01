d81669486ebeeaef913cf3c6c43190ec39ac62ec
TheCodeGoat <110638329+TheCodeGoat@users.noreply.github.com>
2022-08-31 23:23:13 -0700

    Added Discord login

--- components/Auth.js
+++ components/Auth.js
@@ -1,12 +1,23 @@
-import { Button, Heading, VStack } from "@chakra-ui/react";
+import { Button, Heading, HStack, VStack } from "@chakra-ui/react";
 import { supabase } from "../utils/supabaseClient";
-import { FaGithub } from "react-icons/fa";
+import { FaDiscord, FaGithub } from "react-icons/fa";
 
 export default function Auth() {
-  const handleLogin = async () => {
+  const handleGitHubLogin = async () => {
     try {
       const { error } = await supabase.auth.signIn({
         provider: "github",
+      });
+      if (error) throw error;
+    } catch (error) {
+      alert(error.error_description || error.message);
+    }
+  };
+
+  const handleDiscordLogin = async () => {
+    try {
+      const { error } = await supabase.auth.signIn({
+        provider: "discord",
       });
       if (error) throw error;
     } catch (error) {
@@ -18,17 +29,31 @@ export default function Auth() {
     <VStack gap="16px">
       <Heading>Sign In Test</Heading>
 
-      <Button
-        size="lg"
-        colorScheme="gray"
-        leftIcon={<FaGithub />}
-        onClick={(e) => {
-          e.preventDefault();
-          handleLogin();
-        }}
-      >
-        GitHub
-      </Button>
+      <HStack gap="32px">
+        <Button
+          size="lg"
+          colorScheme="gray"
+          leftIcon={<FaGithub />}
+          onClick={(e) => {
+            e.preventDefault();
+            handleGitHubLogin();
+          }}
+        >
+          GitHub
+        </Button>
+
+        <Button
+          size="lg"
+          colorScheme="gray"
+          leftIcon={<FaDiscord />}
+          onClick={(e) => {
+            e.preventDefault();
+            handleDiscordLogin();
+          }}
+        >
+          Discord
+        </Button>
+      </HStack>
     </VStack>
   );
 }
