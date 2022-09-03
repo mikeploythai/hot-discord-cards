9e3d41592fff17f0e1246c09cff3ddbbd582c421
TheCodeGoat <110638329+TheCodeGoat@users.noreply.github.com>
2022-09-03 13:11:28 -0700

    Added delete card function

--- components/Cards.js
+++ components/Cards.js
@@ -36,30 +36,41 @@ export default function Cards() {
     }
   }
 
-  return (
-    <>
-      <HStack gap="32px">
-        {card.map((c) => (
-          <VStack
-            w="250px"
-            minH="350px"
-            borderRadius="16px"
-            padding="16px"
-            gap="8px"
-            shadow="md"
-            transition="0.25s ease-in-out"
-            _hover={{ shadow: "xl" }}
-            key={c.id}
-          >
-            <Flex w="100%" justify="space-between" align="center">
-              <Heading size="sm">{c.name}</Heading>
-              <Text size="sm">{c.attribute}</Text>
-            </Flex>
+  async function deleteCard(id) {
+    try {
+      let { error } = await supabase
+        .from("owners")
+        .delete()
+        .match({ card_id: id });
+      if (error) throw error;
+    } catch (error) {
+      alert(error.error_description || error.message);
+    }
+  }
 
-            <Image src={c.image} alt={c.name} borderRadius="8px" />
-          </VStack>
-        ))}
-      </HStack>
-    </>
+  return (
+    <HStack gap="32px" maxW="1280px" wrap="wrap" justify="center">
+      {card.map((c) => (
+        <VStack
+          w="250px"
+          minH="350px"
+          borderRadius="16px"
+          padding="16px"
+          gap="8px"
+          shadow="md"
+          transition="0.25s ease-in-out"
+          _hover={{ shadow: "xl", cursor: "pointer" }}
+          key={c.id}
+          onClick={() => deleteCard(c.id)}
+        >
+          <Flex w="100%" justify="space-between" align="center">
+            <Heading size="sm">{c.name}</Heading>
+            <Text size="sm">{c.attribute}</Text>
+          </Flex>
+
+          <Image src={c.image} alt={c.name} borderRadius="6px" />
+        </VStack>
+      ))}
+    </HStack>
   );
 }
