2391b42a5cb4d7573159fbde0a3ec4737c9d12da
mploythai <110638329+mploythai@users.noreply.github.com>
2022-09-21 00:58:01 -0700

    Removed unused point refs

--- components/dashboard/profile/index.js
+++ components/dashboard/profile/index.js
@@ -42,7 +42,6 @@ export default function Profile({ getCurrentUser }) {
       if (data) {
         setUsername(data.username);
         setBio(data.bio);
-        setPoints(data.points);
       }
     } catch (error) {
       toast({
@@ -91,7 +90,6 @@ export default function Profile({ getCurrentUser }) {
           >
             {bio}
           </Text>
-         
         </VStack>
       </HStack>
 
