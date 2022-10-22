d125e13a1e61a6a4ab323aa9d9b574ba06b0a39c
mploythai <110638329+mploythai@users.noreply.github.com>
2022-10-21 19:15:30 -0700

    fixed data fetching

--- utils/get-user-data.js
+++ utils/get-user-data.js
@@ -1,18 +1,23 @@
 import { useBreakpointValue, useToast } from "@chakra-ui/react";
-import { useSession, useSupabaseClient } from "@supabase/auth-helpers-react";
-import { useEffect, useState } from "react";
+import {
+  useSession,
+  useSupabaseClient,
+  useUser,
+} from "@supabase/auth-helpers-react";
 import {
   uniqueNamesGenerator,
   adjectives,
   animals,
   NumberDictionary,
 } from "unique-names-generator";
+import { useEffect, useState } from "react";
 import { updateUserData } from "./update-user-data";
 
-export default function getUserData(select, p1, p2) {
-  const [userData, setUserData] = useState(false);
+export default function getUserData() {
   const supabase = useSupabaseClient();
+  const user = useUser();
   const session = useSession();
+  const [userData, setUserData] = useState(false);
   const { updateData } = updateUserData();
 
   const toast = useToast();
@@ -34,30 +39,35 @@ export default function getUserData(select, p1, p2) {
 
   async function getData() {
     try {
-      let { data, error } = await supabase
-        .from("profiles")
-        .select(select)
-        .eq(p1, p2)
-        .maybeSingle();
-      if (error) throw error;
+      if (user) {
+        let { data, error } = await supabase
+          .from("profiles")
+          .select("*")
+          .eq("id", user.id)
+          .maybeSingle();
+        if (error) throw error;
 
-      if (data) setUserData(data);
-      else {
-        const randomNumber = NumberDictionary.generate({ min: 100, max: 999 });
-        const randomName = uniqueNamesGenerator({
-          dictionaries: [adjectives, animals, randomNumber],
-          length: 3,
-          separator: "",
-          style: "capital",
-        });
+        if (data) setUserData(data);
+        else {
+          const randomNumber = NumberDictionary.generate({
+            min: 100,
+            max: 999,
+          });
+          const randomName = uniqueNamesGenerator({
+            dictionaries: [adjectives, animals, randomNumber],
+            length: 3,
+            separator: "",
+            style: "capital",
+          });
 
-        let changes = {
-          username: randomName,
-          bio: "Tap the edit button to change your info!",
-          picture: session.user.user_metadata.picture,
-        };
-        updateData(changes);
-        getData();
+          let changes = {
+            username: randomName,
+            bio: "Tap the edit button to change your info!",
+            picture: session.user.user_metadata.picture,
+          };
+          updateData(changes);
+          getData();
+        }
       }
     } catch (error) {
       toast({
@@ -75,6 +85,5 @@ export default function getUserData(select, p1, p2) {
       return userData;
     }
   }
-
   return { userData };
 }
