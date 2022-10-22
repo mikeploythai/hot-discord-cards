5fb93c3e2a8106c1f00bac2e2fc71b17c7c13e69
mploythai <110638329+mploythai@users.noreply.github.com>
2022-10-21 18:45:20 -0700

    rewrote to accomodate for data load

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,39 @@
-import { Flex } from "@chakra-ui/react";
-import Head from "next/head";
-import Dashboard from "../components/dashboard";
+import { useDisclosure } from "@chakra-ui/react";
+import { useSession, useUser } from "@supabase/auth-helpers-react";
+import { useState } from "react";
+import Profile from "../components/profile";
 import Landing from "../components/general/landing";
+import ProfileCard from "../components/profile/card";
+import EditProfile from "../components/profile/edit";
+import EditProfileForm from "../components/profile/edit/form";
+import getUserData from "../utils/get-user-data";
+import Page from "../components/general/page";
 
-export default function Home({ session }) {
-  return (
-    <Flex minH="100vh" justify="center" p={4} bgColor="gray.100">
-      <Head>
-        <title>Hot Discord Cards {!session ? null : "| Dashboard"}</title>
-      </Head>
-      {!session ? <Landing /> : <Dashboard session={session} />}
-    </Flex>
-  );
+export default function Home() {
+  const session = useSession();
+
+  if (session) {
+    const user = useUser();
+    const { userData } = getUserData("*", "id", user.id);
+    const { isOpen, onOpen, onClose } = useDisclosure();
+    const [disabled, isDisabled] = useState(true);
+
+    return (
+      <Page title="Dashboard">
+        <Profile>
+          <ProfileCard userData={userData} onOpen={onOpen} />
+
+          <EditProfile isOpen={isOpen} onClose={onClose} disabled={disabled}>
+            <EditProfileForm userData={userData} isDisabled={isDisabled} />
+          </EditProfile>
+        </Profile>
+      </Page>
+    );
+  } else {
+    return (
+      <Page>
+        <Landing />
+      </Page>
+    );
+  }
 }
