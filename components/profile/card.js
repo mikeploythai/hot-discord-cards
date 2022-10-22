44b0cf35655004eadd63491b1d6f23dbedc5fac2
mploythai <110638329+mploythai@users.noreply.github.com>
2022-10-21 21:54:21 -0700

    wrapped profile card in container

--- components/profile/card.js
+++ components/profile/card.js
@@ -1,6 +1,7 @@
 import {
   Avatar,
   Button,
+  Container,
   Heading,
   HStack,
   Skeleton,
@@ -12,64 +13,66 @@ import {
 
 export default function ProfileCard({ userData, onOpen, display }) {
   return (
-    <HStack
-      p={{ base: 6, md: 12 }}
-      gap={{ base: 4, md: 8 }}
-      bgColor="white"
-      boxShadow="xs"
-      rounded="lg"
-    >
-      <SkeletonCircle h="100%" w="auto" isLoaded={userData}>
-        <Avatar
-          size={{ base: "lg", md: "xl" }}
-          src={userData.picture}
-          name={userData.username}
-          boxShadow="xs"
-        />
-      </SkeletonCircle>
+    <Container maxW="container.md" p={0}>
+      <HStack
+        p={{ base: 6, md: 12 }}
+        gap={{ base: 4, md: 8 }}
+        bgColor="white"
+        boxShadow="xs"
+        rounded="lg"
+      >
+        <SkeletonCircle h="100%" w="auto" isLoaded={userData}>
+          <Avatar
+            size={{ base: "lg", md: "xl" }}
+            src={userData.picture}
+            name={userData.username}
+            boxShadow="xs"
+          />
+        </SkeletonCircle>
 
-      <VStack w="100%" align="start" gap={2}>
-        <VStack w="100%" align="start">
-          <HStack w="100%" justify="space-between">
-            <Skeleton isLoaded={userData}>
-              <Heading size={{ base: "md", md: "lg" }}>
-                {userData.username}
-              </Heading>
-            </Skeleton>
+        <VStack w="100%" align="start" gap={2}>
+          <VStack w="100%" align="start">
+            <HStack w="100%" justify="space-between">
+              <Skeleton isLoaded={userData}>
+                <Heading size={{ base: "md", md: "lg" }}>
+                  {userData.username}
+                </Heading>
+              </Skeleton>
 
-            <Button
-              display={display}
-              size={{ base: "xs", md: "sm" }}
-              variant="outline"
-              rounded="lg"
-              onClick={onOpen}
-            >
-              Edit
-            </Button>
-          </HStack>
+              <Button
+                display={display}
+                size={{ base: "xs", md: "sm" }}
+                variant="outline"
+                rounded="lg"
+                onClick={onOpen}
+              >
+                Edit
+              </Button>
+            </HStack>
 
-          <SkeletonText w="100%" isLoaded={userData}>
-            <Text
-              fontSize={{ base: "xs", md: "sm" }}
-              wordBreak="break-word"
-              whiteSpace="pre-wrap"
-              noOfLines={5}
-            >
-              {userData.bio}
+            <SkeletonText w="100%" isLoaded={userData}>
+              <Text
+                fontSize={{ base: "xs", md: "sm" }}
+                wordBreak="break-word"
+                whiteSpace="pre-wrap"
+                noOfLines={5}
+              >
+                {userData.bio}
+              </Text>
+            </SkeletonText>
+          </VStack>
+
+          <HStack display="none">
+            <Text fontSize={{ base: "xs", md: "sm" }}>
+              <b>98</b> Following
             </Text>
-          </SkeletonText>
+
+            <Text fontSize={{ base: "xs", md: "sm" }}>
+              <b>1000</b> Followers
+            </Text>
+          </HStack>
         </VStack>
-
-        <HStack display="none">
-          <Text fontSize={{ base: "xs", md: "sm" }}>
-            <b>98</b> Following
-          </Text>
-
-          <Text fontSize={{ base: "xs", md: "sm" }}>
-            <b>1000</b> Followers
-          </Text>
-        </HStack>
-      </VStack>
-    </HStack>
+      </HStack>
+    </Container>
   );
 }
--- components/profile/index.js
+++ components/profile/index.js
@@ -1,13 +1,11 @@
-import { Container, SimpleGrid } from "@chakra-ui/react";
+import { SimpleGrid } from "@chakra-ui/react";
 import EmptySpace from "../general/empty-space";
 
 export default function Profile({ children }) {
   return (
     <SimpleGrid w="100%" h="fit-content" gap={4}>
       <EmptySpace />
-      <Container maxW="container.md" p={0}>
-        {children}
-      </Container>
+      {children}
     </SimpleGrid>
   );
 }
